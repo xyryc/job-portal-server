3dd58a390460da4902dead364058e7c95206fcaf
xyryc <amdanikzz@gmail.com>
2024-12-29 04:45:31 +0600

    added min max salary filter

--- index.js
+++ index.js
@@ -96,6 +96,8 @@ async function run() {
     const email = req.query?.email;
     const sort = req.query?.sort;
     const search = req.query?.search;
+    const min = req.query?.min;
+    const max = req.query?.max;
 
     let query = {};
     let sortQuery = {};
@@ -109,6 +111,14 @@ async function run() {
 
     if (search) {
       query.title = { $regex: search, $options: "i" };
+    }
+
+    if (min && max) {
+      query = {
+        ...query,
+        "salaryRange.min": { $gte: parseInt(min) },
+        "salaryRange.max": { $lte: parseInt(max) },
+      };
     }
 
     const cursor = jobsCollection.find(query).sort(sortQuery);
